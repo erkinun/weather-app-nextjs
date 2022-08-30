829091ad5873f070a3463ea97211891481c4097a
erkin <erkinun@gmail.com>
2022-08-30 20:46:22 +0100

    api for weather

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,7 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 interface WeatherResponse {}
 
-export default (req: NextApiRequest, res: NextApiResponse<WeatherResponse>) => {
-  res.status(200).json({});
-};
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<WeatherResponse>
+) => {
+  const weatherResponse = await fetch(
+    'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=988daee6a2a305a71404a1189e911173&units=metric'
+  )
+  const { main, weather, wind } = await weatherResponse.json()
+
+  const forecast = await fetch(
+    'http://api.openweathermap.org/data/2.5/forecast?q=London,uk&APPID=988daee6a2a305a71404a1189e911173&units=metric'
+  )
+
+  const { list } = await forecast.json()
+
+  res.status(200).json({
+    main,
+    weather,
+    wind,
+    forecast: list.map(({ dt, main, weather }) => ({
+      dt,
+      main,
+      weather,
+    })),
+  })
+}
